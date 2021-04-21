5f9d38c933722c0713548695065a22be2308f9e2
GilbertoASJ <alvesgilberto84@gmail.com>
2021-04-21 18:30:20 -0300

    :sparkles: Updating files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,151 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
+import Link from 'next/link'
+
+import Image from 'next/image'
+import styles from './home.module.scss'
+
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+type Episode = {
+	id: string;
+	title: string;
+	thumbnail: string;
+	members: string;
+	publishedAt: string;
+	duration: number;
+	durationAsString: string;
+	description: string;
+	url: string;
+}
+
+type HomeProps = {
+	latestEpisodes: Episode[];
+	allEpisodes: Episode[];
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-  	<div>
-    	<h1>Index</h1>
-    	<p>{JSON.stringify(props.episodes)}</p>
+  	<div className={styles.homepage}>
+    	
+  		<section className={styles.latestEpisodes}>  
+  			
+  			<h2>Últimos lançamentos</h2>
+
+  			<ul>
+  				{latestEpisodes.map(episode => {
+  					return (
+  						<li key={episode.id}>
+  							<Image 
+  								width={192} 
+  								height={192} 
+  								src={episode.thumbnail} 
+  								alt={episode.title} 
+  								objectFit="cover"
+  							/>
+
+  							<div className={styles.episodeDetails}>
+  							   <Link href={`/episodes/${episode.id}`}>
+  									<a>{episode.title}</a>
+  								</Link>
+  								<p>{episode.members}</p>
+  								<span>{episode.publishedAt}</span>
+  								<span>{episode.durationAsString}</span>
+  							</div>
+
+  							<button type="button"><img src="/play-green.svg" alt="Tocar episódio" /></button>
+  						</li>
+  					)
+  				})}
+  			</ul>
+  		</section>
+
+  		<section className={styles.allEpisodes}>  
+  			
+  			<h2>Todos os episódios</h2>
+
+  			<table cellSpacing={0}>
+  				<thead>
+  					<tr>
+  						<th></th>
+	  					<th>Podcast</th>
+	  					<th>Integrantes</th>
+	  					<th>Data</th>
+	  					<th>Duração</th>
+	  					<th></th>
+	  					</tr>
+  				</thead>
+
+  				<tbody>
+  					{allEpisodes.map(episode => {
+  						return (
+  							<tr key={episode.id}>
+  								<td style={{ width: 72 }}>
+  									<Image 
+  										width={120} 
+		  								height={120} 
+		  								src={episode.thumbnail} 
+		  								alt={episode.title} 
+		  								objectFit="cover"
+  									/>
+  								</td>
+  								<td>
+  								<Link href={`/episodes/${episode.id}`}>
+  									<a>{episode.title}</a>
+  								</Link>
+  								</td>
+  								<td>{episode.members}</td>
+  								<td style={{ width: 100 }}>{episode.publishedAt}</td>
+  								<td>{episode.durationAsString}</td>
+
+  								<td>
+  									<button type="button">
+  										<img src="/play-green.svg" alt="Tocar episódio" />
+  									</button>
+  								</td>
+  							</tr>
+  						)
+  					})}
+  				</tbody>
+  			</table>
+  		</section>
+
     </div>
   )
 }
 
-export async function getStaticProps() {
-	const response = await fetch('http://localhost:3333/episodes')
-	const data = await response.json() 
+export const getStaticProps: GetStaticProps = async () => {
+	const { data } = await api.get('episodes?', {
+		params: {
+			_limit: 12,
+			_sort: 'published_at',
+			_order: 'desc',
+		}
+	})
+
+	const episodes = data.map(episode => {
+		return {
+			id: episode.id,
+			title: episode.title,
+			thumbnail: episode.thumbnail,
+			members: episode.members,
+			publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+			duration: Number(episode.file.duration),
+			durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+			description: episode.description,
+			url: episode.file.url
+		}
+	})
+
+	const latestEpisodes = episodes.slice(0, 2)
+	const allEpisodes = episodes.slice(2, episodes.length)
 
 	return {
 		props: {
-			episodes: data,
+			latestEpisodes,
+			allEpisodes,
 		},
 		revalidate: 60 * 60 * 8,
 	}
